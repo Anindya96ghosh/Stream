bb9933ae0dd7725c3075460629f84e8384ee3a92
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 22:31:11 +0530

    Update Test.java

--- StreamLambdaExpression/com/stream/lamdaexpression/Test.java
+++ StreamLambdaExpression/com/stream/lamdaexpression/Test.java
@@ -8,24 +8,47 @@ public class Test {
 
 	public static void main(String[] args) {
 
+		//Creating ArrayList 
 		ArrayList<Integer> list=new ArrayList<Integer>();
 		
+		//Generating random numbers
 		Random rand=new Random();
 		for(int i=0;i<10000;i++)
 		{
 			list.add(rand.nextInt(100));
-			
 		}
 		
+		//Implementing logic 
 		Predicate<Integer> isEven=number -> number%2==0;
 		Predicate<Integer> isOdd=number -> number%2!=0;
 		Predicate<Integer> isDivisibleBy6=number -> number%6==0;
 		Predicate<Integer> isLessThan12=number -> number<=12;
 		Predicate<Integer> isGreaterThan50=number -> number>=50;
 		
+		//printing the list based on the conditions
 		
-		//list.stream().forEach(System.out::println);
+		System.out.println("All the numbers that are even are ");
 		list.stream().filter(isEven).forEach(System.out::println);
+		
+		
+		System.out.println("=================================================================== ");
+		System.out.println("All the numbers that are odd are ");
+		list.stream().filter(isOdd).forEach(System.out::println);
+		
+		
+		System.out.println("=================================================================== ");
+		System.out.println("All the numbers that are divisible by 6 are ");
+		list.stream().filter(isDivisibleBy6).forEach(System.out::println);
+		
+		
+		System.out.println("=================================================================== ");
+		System.out.println("All the numbers that are less than 12 are ");
+		list.stream().filter(isLessThan12).forEach(System.out::println);
+		
+		
+		System.out.println("=================================================================== ");
+		System.out.println("All the numbers that are greater than 50 are ");
+		list.stream().filter(isGreaterThan50).forEach(System.out::println);
 		
 		
 	}
