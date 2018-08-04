9297357937b4bc0e7b124252277f506813258dc9
Anindya96ghosh <41475961+Anindya96ghosh@users.noreply.github.com>
2018-08-04 22:38:56 +0530

    Update RandomGenerator.java

--- StreamRandom/com/stream/random/RandomGenerator.java
+++ StreamRandom/com/stream/random/RandomGenerator.java
@@ -12,13 +12,17 @@ import com.stream.writeFile.WriteFile;
 public class RandomGenerator {
 	public static void main(String args[]) throws IOException {
 	
+		//Generating Random Numbers
 		Random rand=new Random();
 		Scanner scanner=new Scanner(System.in);
 		
+		
+		//Accepting the path from the users
 		System.out.println("Enter the path");
 		String file_path=scanner.nextLine();
 		
-	
+		//Calling the function to write to a file by passing the file path and the decision 
+		// whether to append to the  existing data 
 		WriteFile write_file=new WriteFile(file_path, true);
 		for(int i=0;i<100;i++)
 		{
@@ -26,16 +30,19 @@ public class RandomGenerator {
 			System.out.println("\n");
 		}
 		
-		
 		Scanner s=new Scanner(new File(file_path));
+		
+		//Creating a tree set
 		Set<Integer> set=new TreeSet<>();
 		
+		//adding numbers to the list
 		while(s.hasNextInt())
 		{
 			set.add(s.nextInt());
 		}
 		s.close();
 		
+		//displaying all the distinct numbers in a sorted manner
 		set.stream().distinct().sorted().forEach(System.out::println);
 	}
 	
